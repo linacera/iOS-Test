aa9e964a257e1e175b4104fe3fafec4b7bde6b9d
Johanna Cerezo <caro.cerezor@gmail.com>
2022-05-19 09:55:17 -0500

    Project creation

---
+++ iOS-Test/iOS-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-Test
+//
+//  Created by lajohanna.cerezo on 19/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-Test/iOS-Test/iOS_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_TestApp.swift
+//  iOS-Test
+//
+//  Created by lajohanna.cerezo on 19/5/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
